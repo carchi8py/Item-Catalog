d0186bc3a43244ddbc58a34d7ca8b9bc79ec813b
Chris Archibald <carchi8py@gmail.com>
2016-05-04 16:51:41 -0700

    Added category.html page

--- vagrant/itemCatalog/finalProject.py
+++ vagrant/itemCatalog/finalProject.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template
 app = Flask(__name__)
 
+catagory = {'name': 'The CRUDdy Crab', 'id': '1'}
 categories = [{'name': 'The CRUDdy Crab', 'id': '1'}, {'name':'Blue Burgers', 'id':'2'},{'name':'Taco Hut', 'id':'3'}]
 
 items = [ {'name':'Cheese Pizza', 'description':'made with fresh cheese', 'price':'$5.99','course' :'Entree', 'id':'1'}, {'name':'Chocolate Cake','description':'made with Dutch Chocolate', 'price':'$3.99', 'course':'Dessert','id':'2'},{'name':'Caesar Salad', 'description':'with fresh organic vegetables','price':'$5.99', 'course':'Entree','id':'3'},{'name':'Iced Tea', 'description':'with lemon','price':'$.99', 'course':'Beverage','id':'4'},{'name':'Spinach Dip', 'description':'creamy dip with fresh spinach','price':'$1.99', 'course':'Appetizer','id':'5'} ]
@@ -13,7 +14,7 @@ def showCatalog():
 
 @app.route('/catalog/<category>/items')
 def showCategory(category):
-    return "This page show all %s" % category
+    return render_template('category.html', categories = categories, catagory = catagory, items = items)    
 
 @app.route('/catalog/<category>/<item>')
 def showItems(category, item):
