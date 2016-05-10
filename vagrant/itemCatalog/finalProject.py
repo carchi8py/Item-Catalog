b3fe5be657ae08a173b76dcd3fd7fdb62eca3f21
Chris Archibald <carchi8py@gmail.com>
2016-05-09 18:38:48 -0700

    Added the ability to add a new item

--- vagrant/itemCatalog/finalProject.py
+++ vagrant/itemCatalog/finalProject.py
@@ -1,8 +1,9 @@
+import datetime
+
 from flask import Flask, render_template, request, redirect, url_for
 from sqlalchemy import create_engine, asc
 from sqlalchemy.orm import sessionmaker
 from database_setup import Catagory, Item, Base
-
 
 app = Flask(__name__)
 
@@ -12,14 +13,6 @@ Base.metadata.bind = engine
 
 DBSession = sessionmaker(bind=engine)
 session = DBSession()
-
-
-
-#catagory = {'name': 'The CRUDdy Crab', 'id': '1'}
-#categories = [{'name': 'The CRUDdy Crab', 'id': '1'}, {'name':'Blue Burgers', 'id':'2'},{'name':'Taco Hut', 'id':'3'}]
-
-#items = [ {'name':'Cheese Pizza', 'description':'made with fresh cheese', 'price':'$5.99','course' :'Entree', 'id':'1'}, {'name':'Chocolate Cake','description':'made with Dutch Chocolate', 'price':'$3.99', 'course':'Dessert','id':'2'},{'name':'Caesar Salad', 'description':'with fresh organic vegetables','price':'$5.99', 'course':'Entree','id':'3'},{'name':'Iced Tea', 'description':'with lemon','price':'$.99', 'course':'Beverage','id':'4'},{'name':'Spinach Dip', 'description':'creamy dip with fresh spinach','price':'$1.99', 'course':'Appetizer','id':'5'} ]
-#item1 =  {'name':'Cheese Pizza','description':'made with fresh cheese','price':'$5.99','course' :'Entree'}
 
 @app.route('/')
 @app.route('/catalog/')
@@ -59,7 +52,6 @@ def editItem(item):
 @app.route('/catalog/<item>/delete', methods=['GET', 'POST'])
 def deleteItem(item):
     item = session.query(Item).filter_by(title = item).one()
-    print 'hi'
     if request.method == 'POST':
         session.delete(item)
         session.commit()
@@ -67,9 +59,25 @@ def deleteItem(item):
     else:
         return render_template('deleteItem.html', item = item)
 
-@app.route('/catalog/item/new')
+@app.route('/catalog/item/new', methods=['GET', 'POST'])
 def newItem():
-    return render_template('newItem.html', categories = categories)
+    categories = session.query(Catagory)
+    if request.method == 'POST':
+        print 'hi'
+        print request.form['catagories']
+        category = session.query(Catagory).filter_by(name = request.form['catagories']).one()
+        print '1'
+        print request.form['title']
+        print request.form['description']
+        newItem = Item(title = request.form['title'], description=request.form['description'], catagory=category, date_added = datetime.datetime.now())
+        print '2'
+        session.add(newItem)
+        print '3'
+        session.commit()
+        print '4'
+        return redirect(url_for('showCatalog'))
+    else:
+        return render_template('newItem.html', categories = categories)
 
 if __name__ == '__main__':
     app.debug = True
